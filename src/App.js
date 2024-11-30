fd2a3cdde50075dd748ce86b55a04544da3bb593
samiyaseen3 <sami.s.yaseen@gmail.com>
2024-11-30 22:38:05 +0300

    add navbar + contact-us + about-us

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import { TodoWrapper } from './components/TodoWrapper';
+import ContactUs from './components/ContactUs';
+import AboutUs from './components/AboutUs';
 
 function App() {
   return (
-    <div className="App">
-      <TodoWrapper  />
-    </div>
+    <Router>
+      <div className="App">
+        {/* Navbar */}
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+            <Link className="navbar-brand" to="/">My App</Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">To-Do List</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/contact-us">Contact Us</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/about-us">About Us</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<TodoWrapper />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+          <Route path="/about-us" element={<AboutUs />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/ContactUs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ContactUs() {
+  return (
+    <div>
+      <h2>Contact Us</h2>
+      <p>This is the Contact Us page.</p>
+    </div>
+  );
+}
+
+export default ContactUs;
